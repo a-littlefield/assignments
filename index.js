d73ce9100b828d1f15c42fed338cb42ca9d6c8fc
Andrew Littlefield <andrew.ray.allan@gmail.com>
2017-05-30 16:57:03 -0600

    changed some js and created touch files.

---
+++ index.js
@@ -0,0 +1,20 @@
+// Question 1
+var x = 9
+var y = 4
+console.log (x+y)
+// Q1 pt B
+var hi = "hello "
+var hello = "hi"
+console.log (hi+hello)
+// Question 2
+var movies = ['Lord of the Rings', 'Edge of tomorrow', 'Planet of the Apes'];
+var games = ['Borderlands', 'Soul Calibur', 'Binding of Isaac'];
+var dailyUse = ['Phone', 'Computer', 'Bed'];
+
+var multiArray = [movies, games, dailyUse];
+console.log (multiArray[2][0]);
+console.log (multiArray[0][1]);
+// Question 3
+var num = prompt ('Enter your number below', '100')
+if (num < 100)
+	alert()
